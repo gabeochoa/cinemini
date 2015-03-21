3a6a6f4782449d1000aaf51a7ca82935e1e42c5e
Gabe <gabe1118@yahoo.com>
2015-03-21 07:08:23 -0400

    Runnable -gabe

--- cinemini.py
+++ cinemini.py
@@ -1,4 +1,28 @@
 import os
-import textprocessor
+import textprocessor as TP
+import videoprocessor as VP
 import wapi
 
+
+def cinemini(inputfile):
+	vidfile, outputsrt = TP.process_srt(inputfile)
+	if(vidfile != None):
+		VP.editfilm(vidfile, outputsrt)
+
+	return
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    import argparse
+
+    parser = argparse.ArgumentParser(description='Generate a compressed version of a film by searching through subtitle tracks.')
+    parser.add_argument('--input', '-i', dest='inputfile', required=True, help='video or subtitle file, or folder')
+    
+    args = parser.parse_args()
+
+    cinemini(args.inputfile)
--- videoprocessor.py
+++ videoprocessor.py
@@ -8,6 +8,7 @@ from moviepy.video.compositing.concatenate import concatenate
 import os
 import re 
 
+#create file at path
 def touch(path):
 	with open(path, 'a'):
 		os.utime(path, None)
@@ -15,6 +16,7 @@ def touch(path):
 
 data = []
 
+#converts from 00:00:00.000 to 0 seconds 
 def cvsecs(time):
 	if (',' not in time) and ('.' not in time):
 		time = time + '.0'
@@ -29,7 +31,7 @@ def cvsecs(time):
 # Load inputfile.mp4 and select the subclip 00:00:50 - 00:00:60
 def clipVideo(inputfile, outputfile, time1, time2):
 	if not os.path.isdir("output/temp"):
-		os.mkdir("output/temp")
+		os.makedirs("output/temp")
 
 	t1 = cvsecs(time1)
 	t2 = cvsecs(time2)
@@ -42,6 +44,7 @@ def clipVideo(inputfile, outputfile, time1, time2):
 
 	return
 
+#combines all videos in output/temp/ to the outfile
 def combineVideos(outputfile):
 	filelist = os.listdir("output/temp/")
 	cliplist = []
@@ -58,6 +61,8 @@ def combineVideos(outputfile):
 
 	return
 
+#Will clip the film into the pieces we want and then 
+#recombine it all at the end into one film. 
 def editfilm(videofile, procsrt):
 	i = 0
 	for timespan in procsrt.keys():
